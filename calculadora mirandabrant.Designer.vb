ed8c6ec3a9e1e9004e496705f48a1f576e2d5662
Brandt-Ignacio <81595011+Brandt-Ignacio@users.noreply.github.com>
2022-10-13 09:49:19 -0300

    Agrego archivos de la calculadora

---
+++ calculadora mirandabrant.Designer.vb
@@ -0,0 +1,273 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.siete = New System.Windows.Forms.Button()
+        Me.dos = New System.Windows.Forms.Button()
+        Me.ocho = New System.Windows.Forms.Button()
+        Me.uno = New System.Windows.Forms.Button()
+        Me.cero = New System.Windows.Forms.Button()
+        Me.cinco = New System.Windows.Forms.Button()
+        Me.tres = New System.Windows.Forms.Button()
+        Me.seis = New System.Windows.Forms.Button()
+        Me.nueve = New System.Windows.Forms.Button()
+        Me.cuatro = New System.Windows.Forms.Button()
+        Me.menos = New System.Windows.Forms.Button()
+        Me.mas = New System.Windows.Forms.Button()
+        Me.coma = New System.Windows.Forms.Button()
+        Me.dividir = New System.Windows.Forms.Button()
+        Me.multiplicar = New System.Windows.Forms.Button()
+        Me.calculo = New System.Windows.Forms.TextBox()
+        Me.borrar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.25!)
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Button1.Location = New System.Drawing.Point(62, 338)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(60, 60)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "="
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'siete
+        '
+        Me.siete.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.siete.Location = New System.Drawing.Point(62, 140)
+        Me.siete.Name = "siete"
+        Me.siete.Size = New System.Drawing.Size(60, 60)
+        Me.siete.TabIndex = 1
+        Me.siete.Text = "7"
+        Me.siete.UseVisualStyleBackColor = True
+        '
+        'dos
+        '
+        Me.dos.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.dos.Location = New System.Drawing.Point(128, 272)
+        Me.dos.Name = "dos"
+        Me.dos.Size = New System.Drawing.Size(60, 60)
+        Me.dos.TabIndex = 2
+        Me.dos.Text = "2"
+        Me.dos.UseVisualStyleBackColor = True
+        '
+        'ocho
+        '
+        Me.ocho.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.ocho.Location = New System.Drawing.Point(128, 140)
+        Me.ocho.Name = "ocho"
+        Me.ocho.Size = New System.Drawing.Size(60, 60)
+        Me.ocho.TabIndex = 3
+        Me.ocho.Text = "8"
+        Me.ocho.UseVisualStyleBackColor = True
+        '
+        'uno
+        '
+        Me.uno.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.uno.Location = New System.Drawing.Point(62, 272)
+        Me.uno.Name = "uno"
+        Me.uno.Size = New System.Drawing.Size(60, 60)
+        Me.uno.TabIndex = 4
+        Me.uno.Text = "1"
+        Me.uno.UseVisualStyleBackColor = True
+        '
+        'cero
+        '
+        Me.cero.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.cero.Location = New System.Drawing.Point(128, 338)
+        Me.cero.Name = "cero"
+        Me.cero.Size = New System.Drawing.Size(60, 60)
+        Me.cero.TabIndex = 5
+        Me.cero.Text = "0"
+        Me.cero.UseVisualStyleBackColor = True
+        '
+        'cinco
+        '
+        Me.cinco.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.cinco.Location = New System.Drawing.Point(128, 206)
+        Me.cinco.Name = "cinco"
+        Me.cinco.Size = New System.Drawing.Size(60, 60)
+        Me.cinco.TabIndex = 6
+        Me.cinco.Text = "5"
+        Me.cinco.UseVisualStyleBackColor = True
+        '
+        'tres
+        '
+        Me.tres.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.tres.Location = New System.Drawing.Point(194, 272)
+        Me.tres.Name = "tres"
+        Me.tres.Size = New System.Drawing.Size(60, 60)
+        Me.tres.TabIndex = 7
+        Me.tres.Text = "3"
+        Me.tres.UseVisualStyleBackColor = True
+        '
+        'seis
+        '
+        Me.seis.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.seis.Location = New System.Drawing.Point(194, 206)
+        Me.seis.Name = "seis"
+        Me.seis.Size = New System.Drawing.Size(60, 60)
+        Me.seis.TabIndex = 8
+        Me.seis.Text = "6"
+        Me.seis.UseVisualStyleBackColor = True
+        '
+        'nueve
+        '
+        Me.nueve.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.nueve.Location = New System.Drawing.Point(194, 140)
+        Me.nueve.Name = "nueve"
+        Me.nueve.Size = New System.Drawing.Size(60, 60)
+        Me.nueve.TabIndex = 9
+        Me.nueve.Text = "9"
+        Me.nueve.UseVisualStyleBackColor = True
+        '
+        'cuatro
+        '
+        Me.cuatro.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.cuatro.Location = New System.Drawing.Point(62, 206)
+        Me.cuatro.Name = "cuatro"
+        Me.cuatro.Size = New System.Drawing.Size(60, 60)
+        Me.cuatro.TabIndex = 10
+        Me.cuatro.Text = "4"
+        Me.cuatro.UseVisualStyleBackColor = True
+        '
+        'menos
+        '
+        Me.menos.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.25!)
+        Me.menos.Location = New System.Drawing.Point(260, 272)
+        Me.menos.Name = "menos"
+        Me.menos.Size = New System.Drawing.Size(60, 60)
+        Me.menos.TabIndex = 11
+        Me.menos.Text = "-"
+        Me.menos.UseVisualStyleBackColor = True
+        '
+        'mas
+        '
+        Me.mas.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.25!)
+        Me.mas.Location = New System.Drawing.Point(260, 338)
+        Me.mas.Name = "mas"
+        Me.mas.Size = New System.Drawing.Size(60, 60)
+        Me.mas.TabIndex = 12
+        Me.mas.Text = "+"
+        Me.mas.UseVisualStyleBackColor = True
+        '
+        'coma
+        '
+        Me.coma.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!)
+        Me.coma.Location = New System.Drawing.Point(194, 338)
+        Me.coma.Name = "coma"
+        Me.coma.Size = New System.Drawing.Size(60, 60)
+        Me.coma.TabIndex = 13
+        Me.coma.Text = ","
+        Me.coma.UseVisualStyleBackColor = True
+        '
+        'dividir
+        '
+        Me.dividir.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.dividir.Location = New System.Drawing.Point(260, 140)
+        Me.dividir.Name = "dividir"
+        Me.dividir.Size = New System.Drawing.Size(60, 60)
+        Me.dividir.TabIndex = 15
+        Me.dividir.Text = "%"
+        Me.dividir.UseVisualStyleBackColor = True
+        '
+        'multiplicar
+        '
+        Me.multiplicar.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!)
+        Me.multiplicar.Location = New System.Drawing.Point(260, 206)
+        Me.multiplicar.Name = "multiplicar"
+        Me.multiplicar.Size = New System.Drawing.Size(60, 60)
+        Me.multiplicar.TabIndex = 16
+        Me.multiplicar.Text = "X"
+        Me.multiplicar.UseVisualStyleBackColor = True
+        '
+        'calculo
+        '
+        Me.calculo.Font = New System.Drawing.Font("Microsoft Sans Serif", 37.25!)
+        Me.calculo.Location = New System.Drawing.Point(62, 65)
+        Me.calculo.Multiline = True
+        Me.calculo.Name = "calculo"
+        Me.calculo.Size = New System.Drawing.Size(258, 60)
+        Me.calculo.TabIndex = 17
+        '
+        'borrar
+        '
+        Me.borrar.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.25!)
+        Me.borrar.Location = New System.Drawing.Point(326, 140)
+        Me.borrar.Name = "borrar"
+        Me.borrar.Size = New System.Drawing.Size(75, 60)
+        Me.borrar.TabIndex = 18
+        Me.borrar.Text = "BORRAR"
+        Me.borrar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(434, 505)
+        Me.Controls.Add(Me.borrar)
+        Me.Controls.Add(Me.calculo)
+        Me.Controls.Add(Me.multiplicar)
+        Me.Controls.Add(Me.dividir)
+        Me.Controls.Add(Me.coma)
+        Me.Controls.Add(Me.mas)
+        Me.Controls.Add(Me.menos)
+        Me.Controls.Add(Me.cuatro)
+        Me.Controls.Add(Me.nueve)
+        Me.Controls.Add(Me.seis)
+        Me.Controls.Add(Me.tres)
+        Me.Controls.Add(Me.cinco)
+        Me.Controls.Add(Me.cero)
+        Me.Controls.Add(Me.uno)
+        Me.Controls.Add(Me.ocho)
+        Me.Controls.Add(Me.dos)
+        Me.Controls.Add(Me.siete)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents siete As System.Windows.Forms.Button
+    Friend WithEvents dos As System.Windows.Forms.Button
+    Friend WithEvents ocho As System.Windows.Forms.Button
+    Friend WithEvents uno As System.Windows.Forms.Button
+    Friend WithEvents cero As System.Windows.Forms.Button
+    Friend WithEvents cinco As System.Windows.Forms.Button
+    Friend WithEvents tres As System.Windows.Forms.Button
+    Friend WithEvents seis As System.Windows.Forms.Button
+    Friend WithEvents nueve As System.Windows.Forms.Button
+    Friend WithEvents cuatro As System.Windows.Forms.Button
+    Friend WithEvents menos As System.Windows.Forms.Button
+    Friend WithEvents mas As System.Windows.Forms.Button
+    Friend WithEvents coma As System.Windows.Forms.Button
+    Friend WithEvents dividir As System.Windows.Forms.Button
+    Friend WithEvents multiplicar As System.Windows.Forms.Button
+    Friend WithEvents calculo As System.Windows.Forms.TextBox
+    Friend WithEvents borrar As System.Windows.Forms.Button
+
+End Class
---
+++ calculadora mirandabrant.vb
@@ -0,0 +1,80 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles siete.Click
+        calculo.Text += "7"
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ocho.Click
+        calculo.Text += "8"
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles dos.Click
+        calculo.Text += "2"
+    End Sub
+
+    Private Sub Button15_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub cero_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cero.Click
+        calculo.Text += "0"
+    End Sub
+
+    Private Sub nueve_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles nueve.Click
+        calculo.Text += "9"
+    End Sub
+
+    Private Sub dividir_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles dividir.Click
+        calculo.Text += "%"
+    End Sub
+
+    Private Sub cuatro_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cuatro.Click
+        calculo.Text += "4"
+    End Sub
+
+    Private Sub cinco_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cinco.Click
+        calculo.Text += "5"
+    End Sub
+
+    Private Sub seis_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles seis.Click
+        calculo.Text += "6"
+    End Sub
+
+    Private Sub uno_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles uno.Click
+        calculo.Text += "1"
+    End Sub
+
+    Private Sub tres_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tres.Click
+        calculo.Text += "3"
+    End Sub
+
+    Private Sub menos_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles menos.Click
+        calculo.Text += "-"
+    End Sub
+
+    Private Sub multiplicar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles multiplicar.Click
+        calculo.Text += "x"
+    End Sub
+
+    Private Sub mas_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles mas.Click
+        calculo.Text += "+"
+    End Sub
+
+    Private Sub coma_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles coma.Click
+        If InStr(coma.Text, ".", CompareMethod.Text) = 0 Then
+            calculo.Text += ","
+        End If
+    End Sub
+
+    Private Sub borrar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles borrar.Click
+        calculo.Text = ""
+    End Sub
+End Class
